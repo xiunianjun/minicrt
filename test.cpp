07fabd6dad6053026b4dacd7ffea220764f02844
xiunian <nniferyy@gmail.com>
2023-09-17 14:36:10 +0000

    add cpp rt lib test

---
+++ test.cpp
@@ -0,0 +1,11 @@
+// test.cpp
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char* argv[])
+{
+ string* msg = new string(“Hello World”);
+ cout << *msg << endl;
+ delete msg;
+ return 0;
+}
